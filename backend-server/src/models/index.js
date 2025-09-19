25c6602e3c29a163cbb2dc82888719fe2d31b285
Khaled-spec <khaled@lpx.ae>
2025-09-19 15:16:32 +0400

    Replace repository with current local directory

--- backend-server/src/models/index.js
+++
@@ -1,9 +0,0 @@
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-
-
-module.exports.Notification = require('./notification.model');
-module.exports.TermsAndCondition = require('./termsConditions.model');
-module.exports.AboutUs = require('./aboutUs.model');
-module.exports.PrivacyPolicy = require('./privacyPolicy.model');
-module.exports.Support = require('./support.model');
